734ede6c08bd5e65def7b99916c9b3386ce1bfd2
samirhassen <hassen.samir@gmail.com>
2015-06-24 19:46:33 +0300

    step

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Samir\Documents\Internett Stuff\Webbsites\Wasmiah Kunder\HafrJalyat\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Samir\Documents\Internett Stuff\Webbsites\Wasmiah Kunder\HafrJalyat\platforms\android\AndroidManifest.xml \
+C:\Users\Samir\Documents\Internett Stuff\Webbsites\Wasmiah Kunder\SM2015\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Samir\Documents\Internett Stuff\Webbsites\Wasmiah Kunder\SM2015\platforms\android\AndroidManifest.xml \
